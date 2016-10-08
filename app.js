41ee247721e449d31edda4da15572de95f725414
Pedro Bernardes <phec06@gmail.com>
2016-10-08 13:09:07 -0300

    Bug fixes

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
-    (function(){
-      var app = angular.module('Todo-app', []);
-        app.controller('TodoCtrl', function($scope){
-            $scope.todos= [];
+init = function() {
+    var app = angular.module('TodoApp', []);
+    app.controller('TodoCtrl', function($scope){
+        $scope.todos= [];
 
-            $scope.todoPriority= 'normal';
+        $scope.todoPriority= 'normal';
 
-            $scope.addTodo= function(){
-              var newTodo={
+        $scope.addTodo= function(){
+            var newTodo={
                 done: false,
                 text: $scope.todoText,
                 priority: $scope.todoPriority
-              };
-                $scope.todos.push(newTodo);
-                $scope.todoText = '';
-                $scope.todoPriority = 'normal';
             };
-
-        $scope.getTotal= function(){
-          return $scope.todos.length;
+            $scope.todos.push(newTodo);
+            $scope.todoText = '';
+            $scope.todoPriority = 'normal';
         };
 
-       $scope.removeTodo= function(start){
+        $scope.getTotal= function(){
+            return $scope.todos.length;
+        };
 
-          $scope.todos.splice(start, 1);
+        $scope.removeTodo= function(start){
+
+            $scope.todos.splice(start, 1);
         };
 
         $scope.move= function(index, direction){
-          if (direction === 'up'){
-            if (index === 0){
-              return;
-              }
-              index = index -1;
+            if (direction === 'up'){
+                if (index === 0){
+                    return;
+                }
+                index = index -1;
             }
-          if (direction === 'down'){
-             if (index === $scope.todos.length -1) {
-             return;
-             }
-          }
-        var todo= $scope.todos[index];
-        $scope.todos.splice(index+2,0, todo);
-        $scope.todos.splice(index, 1);
+            if (direction === 'down'){
+                if (index === $scope.todos.length -1) {
+                    return;
+                }
+            }
+            var todo= $scope.todos[index];
+            $scope.todos.splice(index+2,0, todo);
+            $scope.todos.splice(index, 1);
         };
-     });
-
     });
+};
+
+init();
